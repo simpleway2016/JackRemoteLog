289ba88630b0666904cf7f2b858bf634f4f8e223
Jack <Jack@DESKTOP-0OEJK3T>
2023-12-27 16:14:59 +0800

    支持多个context同时查询

--- TestWebApi/MyLogFilter.cs
+++ TestWebApi/MyLogFilter.cs
@@ -4,23 +4,10 @@ namespace TestWebApi
 {
     public class MyLogFilter : ILogItemFilter
     {
-        IHttpContextAccessor _httpContextAccessor;
-        public MyLogFilter(IHttpContextAccessor httpContextAccessor)
-        {
-            this._httpContextAccessor = httpContextAccessor;
-
-        }
+      public static AsyncLocal<string> TrackId { get; set; } = new AsyncLocal<string>();
         public void OnExecuting(LogItem logItem)
         {
-            var context= _httpContextAccessor.HttpContext;
-            if (context == null)
-            {
-                
-            }
-            else
-            {
-                logItem.TraceId = context.Request.Headers.Host;
-            }
+            logItem.TraceId = TrackId.Value;
         }
     }
 }
--- TestWebApi/Program.cs
+++ TestWebApi/Program.cs
@@ -4,6 +4,22 @@ using TestWebApi;
 var builder = WebApplication.CreateBuilder(args);
 Console.WriteLine(builder.Configuration["Logging"]);
 
+ConfigurationBuilder builder2 = new ConfigurationBuilder();
+builder2.AddJsonFile("appsettings2.json", optional: true, reloadOnChange: true);
+var configuration2 =  builder2.Build();
+
+var myServices = new ServiceCollection();
+myServices.AddLogging(b =>
+{
+    b.UseJackRemoteLogger(configuration2, new Options
+    {
+        UserName = "JACK",
+        Password = "123"
+    });
+});
+myServices.AddSingleton<ILogItemFilter, MyLogFilter>();
+var serviceProvider2 = myServices.BuildServiceProvider();
+
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddSingleton<ILogItemFilter,MyLogFilter> ();
 builder.Services.AddLogging(b =>
@@ -24,7 +40,11 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Use((c, next) => {
-    c.RequestServices.GetService<ILogger<Program>>().LogInformation("有访问");
+    MyLogFilter.TrackId.Value = Guid.NewGuid().ToString();
+    c.RequestServices.GetService<ILogger<Program>>().LogInformation($"有访问-{c.Request.Path}");
+    serviceProvider2.GetService<ILogger<Program>>().LogInformation($"有访问-{c.Request.Host}");
+
+    MyLogFilter.TrackId.Value = null;
     return next();
 });
 
