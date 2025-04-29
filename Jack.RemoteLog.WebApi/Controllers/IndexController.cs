6bf2216bb35d35f4bc0e9de5e76c34313765fa81
RunYu2014 <896872647@qq.com>
2025-04-29 18:17:59 +0800

    add Title 配置

--- Jack.RemoteLog.WebApi/Controllers/IndexController.cs
+++ Jack.RemoteLog.WebApi/Controllers/IndexController.cs
@@ -11,7 +11,11 @@ namespace Jack.RemoteLog.WebApi.Controllers
         public ContentResult Get()
         {
             var path = Path.Combine("wwwroot", "index.html");
+            var title = Global.Configuration["Title"];
             var htmlContent = System.IO.File.ReadAllText(path, Encoding.UTF8);
+            if (!string.IsNullOrEmpty(title))
+                htmlContent = htmlContent.Replace("<title>Vela</title>", $"<title>{title}</title>");
+
             return Content(htmlContent, "text/html");
         }
     }
